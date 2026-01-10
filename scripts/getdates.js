b2f2a9a745a969c151a80090adc2831983773b98
Mardo35 <mardocheebaguidy106@gmail.com>
2026-01-09 20:55:07 -0500

    week 01 assignment

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// last date modified
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
+// Current year
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
